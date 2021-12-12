58006efcd171af0bc54700be225c5e489eda8134
Alzidan <zidanamiraz5@gmail.com>
2021-12-12 17:47:17 +0700

    branching

---
+++ app/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index()
+    {
+        if (session()->get('logged_in')){
+            //echo "Ini adalah halaman user";
+            echo view('dashboard_view');
+        }
+        else{
+            echo "Invalid";
+            return redirect()->to(base_url('/login'))->with('error', "Invalid Credential");
+        }
+    }
+    
+}
