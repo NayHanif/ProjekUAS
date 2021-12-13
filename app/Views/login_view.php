9321613360491fcba297d54c629dfcf9780db6f7
kevinopee <kevinchristoper00@gmail.com>
2021-12-13 23:12:30 +0700

    edit views & css

--- app/Views/login_view.php
+++ app/Views/login_view.php
@@ -1,35 +1,32 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class="container mt-3">
-
-  <div class="row pt-5">
-    <div class="col"></div>
-    <div class="col p-3 border border-success">
-      <h2>Login User</h2>
-      <p>Masukkan username dan password pada formulir di bawah ini:</p>
-      <?php if (!empty(session()->getFlashdata('error'))) : ?>
-        <div class="alert alert-warning alert-dismissible">
-          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-          <?= session()->getFlashdata('error'); ?>
-        </div>
-      <?php endif; ?>
-      <form action="<?= base_url() ?>/login/submitLogin" method="POST">
-        <div class="form-floating mb-3 mt-3">
-          <input type="text" class="form-control" id="username" placeholder="Masukkan username" name="username">
-          <label for="email">Username</label>
-        </div>
-        <div class="form-floating mt-3 mb-3">
-          <input type="password" class="form-control" id="password" placeholder="Masukkan Password" name="password">
-          <label for="password">Password</label>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <a href="<?= base_url() ?>/register">Belum punya akun?</a>
+<div class="container-fluid mt-3 d-flex flex-column justify-content-center align-items-center">
+  <?php if (!empty(session()->getFlashdata('error'))) : ?>
+    <div class="alert alert-warning alert-dismissible">
+      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      <?= session()->getFlashdata('error'); ?>
     </div>
-    <div class="col"></div>
-
-  </div>
+  <?php endif; ?>
+  <form action="<?= base_url() ?>/login/submitLogin" method="POST">
+    <div class="container shadow rounded frames border border-5 border-success pt-3">
+      <h2 class="text-center">Login</h2>
+      <div class="row">
+        <div class="mb-3 mt-3">
+          <input type="text" class="form-control" id="username" placeholder="Username" name="username">
+        </div>
+        <div class="mt-3 mb-3">
+          <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+        </div>
+        <div class="container-fluid d-flex justify-content-center my-3">
+          <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+        <div class="mb-3 text-center">
+          <a href="<?= base_url() ?>/register">Belum punya akun?</a>
+        </div>
+      </div>
+    </div>
+  </form>
 
 </div>
 <?= $this->endSection(); ?>
--- app/Views/register_view.php
+++ app/Views/register_view.php
@@ -1,46 +1,56 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<div class="container mt-3">
-  <div class="row">
-    <div class="col"></div>
-    <div class="col p-3 border border-success">
-      <h2>Registrasi User</h2>
-      <p>Lengkapi isian formulir di bawah ini:</p>
-      <?php if (!empty(session()->getFlashdata('error'))) : ?>
-        <div class="alert alert-danger alert-dismissible">
-          <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-          <?= \Config\Services::validation()->listErrors(); ?>
-        </div>
-      <?php endif; ?>
-      <form action="<?= base_url() ?>/register/submitRegister" method="POST">
-        <div class="form-floating mb-3 mt-3">
-          <input type="text" class="form-control" id="username" placeholder="Masukkan username" name="username" value="<?= old('username') ?>">
-          <label for="email">Username</label>
-        </div>
-        <div class="form-floating mt-3 mb-3">
-          <input type="password" class="form-control" id="password" placeholder="Masukkan Password" name="password">
-          <label for="password">Password</label>
-        </div>
-        <div class="form-floating mt-3 mb-3">
-          <input type="password" class="form-control" id="cpassword" placeholder="Masukkan Konfirmasi Password" name="cpassword">
-          <label for="cpassword">Konfirmasi Password</label>
-        </div>
-        <div class="form-floating mt-3 mb-3">
-          <input type="text" class="form-control" id="namadepan" placeholder="Masukkan Nama Depan" name="namadepan" value="<?= old('namadepan') ?>">
-          <label for="namadepan">Nama Depan</label>
-        </div>
-        <div class="form-floating mt-3 mb-3">
-          <input type="text" class="form-control" id="namabelakang" placeholder="Masukkan Nama Depan" name="namabelakang" value="<?= old('namabelakang') ?>">
-          <label for="namabelakang">Nama Belakang</label>
-        </div>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </form>
-      <a href="<?= base_url() ?>/login">Sudah punya akun?</a>
+<div class="container-fluid mt-3 d-flex flex-column justify-content-center align-items-center">
+  <?php if (!empty(session()->getFlashdata('error'))) : ?>
+    <div class="alert alert-danger alert-dismissible">
+      <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+      <?= \Config\Services::validation()->listErrors(); ?>
     </div>
-    <div class="col"></div>
-  </div>
+  <?php endif; ?>
+  <form action="<?= base_url() ?>/register/submitRegister" method="POST">
+    <div class="container shadow rounded frames border border-5 border-success pt-3">
+      <h2 class="text-center">Register</h2>
+      <div class="row">
+        <div class="col-12">
+          <div class="mb-3 mt-3">
+            <input type="text" class="form-control" id="username" placeholder="Username" name="username" value="<?= old('username') ?>">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="mt-3 mb-3">
+            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="mt-3 mb-3">
+            <input type="password" class="form-control" id="cpassword" placeholder="Konfirmasi Password" name="cpassword">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="mt-3 mb-3">
+            <input type="text" class="form-control" id="namadepan" placeholder="Nama Depan" name="namadepan" value="<?= old('namadepan') ?>">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="mt-3 mb-3">
+            <input type="text" class="form-control" id="namabelakang" placeholder="Nama Belakang" name="namabelakang" value="<?= old('namabelakang') ?>">
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="container-fluid d-flex justify-content-center my-3">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="mb-3 text-center">
+            <a href="<?= base_url() ?>/login">Sudah punya akun?</a>
+          </div>
+        </div>
 
+      </div>
+    </div>
+  </form>
 </div>
 
 <?= $this->endSection(); ?>
